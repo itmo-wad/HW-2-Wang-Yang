01fb19a4205628b56828ca03a7dea7851f958482
StefanSnow <wang122001283@gmail.com>
2021-04-14 02:16:05 +0300

    Add files via upload

---
+++ hw2-wang-yang/server_hw2.py
@@ -0,0 +1,15 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')  
+
+@app.route('/img/<path:path>')
+def img(path):
+    return send_from_directory('static\\img', path)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
